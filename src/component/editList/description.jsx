1820f50791e312f0bf70ea5d5f96cd45b2ef737c
Adam <adam@example.com>
2020-02-09 14:20:00 -0700

    edit list styling the description.

--- src/component/editList/description.jsx
+++ src/component/editList/description.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 const Description = ({ description, onDescriptionChange }) => {
   return (
-    <div className="row">
-      <input
-        onChange={e => onDescriptionChange(e.target.value)}
-        value={description}
-        placeholder="description"
-      />
+    <div className="row mb-3">
+      <div className="col" style={{ textAlign: "center" }}>
+        <input
+          style={{ textAlign: "center" }}
+          placeholder="Title"
+          type="text"
+          className="input-field input-title input-description"
+          onChange={e => onDescriptionChange(e.target.value)}
+          value={description}
+          placeholder="description"
+          maxLength="50"
+        />
+      </div>
     </div>
   );
 };
